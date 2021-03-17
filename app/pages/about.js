857afe18b62a5e8fe57a875514dbf605d2a93079
Saulo Junior <36682208+SauloJuniorsj@users.noreply.github.com>
2021-03-17 03:28:37 -0300

    Update about.js

--- app/pages/about.js
+++ app/pages/about.js
@@ -2,7 +2,7 @@ export const WelcomeComponent = {
     template:
         `
         <div class="min-h-screen bg-gray-100 bg-no-repeat bg-cover" style="background-image: url('app/images/image.jpg')">
-            <div style="text-align: center; padding-top: 17em; padding-right: 14em;">
+            <div style="text-align: center; padding-top: 15em; padding-right: 14em;">
                 <router-link to="/list" class="font-black text-4xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 "> Ir para listagem</router-link>
             </div>   
     
